facef2bb2100d0e42ff44c4c9d59c557c6674b86
gitarena <allan.fernandes@gmail.com>
2021-12-19 11:37:13 -0300

    Add icpm rule in security group

--- deploy_jenkins/terraform/security-group.tf
+++ deploy_jenkins/terraform/security-group.tf
@@ -16,6 +16,12 @@ resource "aws_security_group" "sg_jenkins" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
    egress {
     from_port       = 0
