4480c5a700e9e2db789f15997b7b87625ca62f32
gitarena <allan.fernandes@gmail.com>
2021-12-23 16:08:42 -0300

    Correcting resource aws_key_pair

--- deploy_webservers/terraform/Windows.tf
+++ deploy_webservers/terraform/Windows.tf
@@ -9,16 +9,16 @@ resource "aws_instance" "webserver-win" {
   key_name = "ubuntu"
 }
 
-resource "aws_key_pair" "webserver" {
+resource "aws_key_pair" "webserver_windows" {
   key_name = "ubuntu"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCU3FvpHEwefi2nJsWRKnnFRVAkLOPyCqRb8F7QzoVqVm8Bfe8kHzeIVdfmEeuO6SfM51MjDxY5tse3AE0S5Sb7nG6GnHuLQXOjd6IN1iHmB1tMC9Sg0vymtgGqSuPr9Y5RBRGMt9bPgT3aOCDs26WHUOquZ0teYpip0wDiKKJbKinY++tm+KH7r5VBrLVv5G5Iqt/MOb0WPKJz2UFP+Uc3AUAzC7B2k8GAQexVAgksHER9Xc6JgXGjfaeErDMUcp0gNKeGoHCjEXHdZjHuNmoohyK63EWU+mWkIybp2o6mREapMzhQzDLxge8zA6RSwhWXXmPhzYo89Gyn4GjrI5px ec2-user"
 }
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "webserver_windows" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "main"
+    Name = "webserver_windows"
   }
 }
