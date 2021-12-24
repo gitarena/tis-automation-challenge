4e0a9a541c20cdb1c2379e979ca11b5dba6d8263
gitarena <allan.fernandes@gmail.com>
2021-12-24 00:16:46 -0300

    adjust webserver windows name

--- deploy_webservers/terraform/output.tf
+++ deploy_webservers/terraform/output.tf
@@ -2,5 +2,5 @@ output "dns_name_webserver_linux" {
   value = ["${aws_instance.webserver-linux.*.public_dns}"]
 }
 output "dns_name_webserver_windows" {
-  value = ["${aws_instance.webserver-windows.*.public_dns}"]
+  value = ["${aws_instance.webserver-win.*.public_dns}"]
 }
